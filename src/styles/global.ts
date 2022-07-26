42d8df4f2d087c30d6fcba5c2f78bfb8f154a646
thiago lopes de mello <timabuntu@gmail.com>
2022-07-26 10:13:02 -0300

    style: defined font

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,5 +1,21 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
- body {background-color: #333;}
+ 
+ * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+ }
+
+ body {
+  background: #333;
+  color: #fff
+ }
+
+ body, input, text-area, button {
+  font-family: 'Roboto', sans-serif;
+  font-weight: 400;
+  font-size: 1rem;
+ }
  `;
