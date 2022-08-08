c4254751e24d8281fdc734440890872190c21a4d
thiago lopes de mello <timabuntu@gmail.com>
2022-08-08 13:15:30 -0300

    chore: removed button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
 
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes'
@@ -7,10 +6,6 @@ import { defaultTheme } from './styles/themes'
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button colorVariant="default" />
-      <Button colorVariant="primary" />
-      <Button colorVariant="secondary" />
-      <Button />
       <GlobalStyle />
     </ThemeProvider>
   )
