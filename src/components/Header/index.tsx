1069817418b898c785d3697b572fd19e3b9f3cc5
thiago lopes de mello <timabuntu@gmail.com>
2022-08-09 15:33:18 -0300

    feat: added title link for accessibility

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,10 +8,10 @@ export function Header() {
     <HeaderContainer>
       <img src={LogoTomato} alt="logotipo tomate" />
       <nav>
-        <NavLink to="/">
+        <NavLink to="/" title="Timer">
           <Timer size={24} />
         </NavLink>
-        <NavLink to="/history">
+        <NavLink to="/history" title="Histórico">
           <Scroll size={24} />
         </NavLink>
       </nav>
