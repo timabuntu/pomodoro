58dc3ba362b4e2bf26003e2e37d44e8e30e14f37
thiago lopes de mello <timabuntu@gmail.com>
2022-08-09 15:04:45 -0300

    feat: created component header

--- src/components/Layout/DefaultLayout/index.tsx
+++ src/components/Layout/DefaultLayout/index.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom'
+import { Header } from '../../Header'
 import { LayoutContainer } from './style'
 
 export function DefaultLayout() {
   return (
     <LayoutContainer>
+      <Header />
       <Outlet />
     </LayoutContainer>
   )
