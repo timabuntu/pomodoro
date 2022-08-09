2e8308dc63441f283caa74f18d051e9915197fcf
thiago lopes de mello <timabuntu@gmail.com>
2022-08-09 15:37:03 -0300

    style: stylized active link

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -30,6 +30,10 @@ export const HeaderContainer = styled.div`
       &:hover {
         border-bottom: 3px solid ${(props) => props.theme['blue-500']};
       }
+
+      &.active {
+        color: ${(props) => props.theme['blue-500']};
+      }
     }
   }
 `
