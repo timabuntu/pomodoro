b9d34f8abe80e542ad8ef8dd9f2974bfe02c0292
thiago lopes de mello <timabuntu@gmail.com>
2022-08-09 15:27:21 -0300

    style: styled icons created

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -4,4 +4,32 @@ export const HeaderContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+
+  img {
+    width: 48px;
+    height: 48px;
+  }
+
+  nav {
+    display: flex;
+    gap: 0.5rem;
+
+    a {
+      width: 3rem;
+      height: 3rem;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      color: ${(props) => props.theme['gray-100']};
+
+      border-top: 3px solid transparent;
+      border-bottom: 3px solid transparent;
+
+      &:hover {
+        border-bottom: 3px solid ${(props) => props.theme['blue-500']};
+      }
+    }
+  }
 `
