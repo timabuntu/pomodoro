b75f1e891df47dec7f2ea898848de412038bcb9c
thiago lopes de mello <timabuntu@gmail.com>
2022-08-09 15:31:16 -0300

    chore: changed element anchor to navlink component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,17 +2,18 @@ import { HeaderContainer } from './styles'
 
 import LogoTomato from '../../assets/tomato.png'
 import { Scroll, Timer } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
 export function Header() {
   return (
     <HeaderContainer>
       <img src={LogoTomato} alt="logotipo tomate" />
       <nav>
-        <a href="">
+        <NavLink to="/">
           <Timer size={24} />
-        </a>
-        <a href="">
+        </NavLink>
+        <NavLink to="/history">
           <Scroll size={24} />
-        </a>
+        </NavLink>
       </nav>
     </HeaderContainer>
   )
