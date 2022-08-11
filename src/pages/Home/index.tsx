3b04a445b5360a1de7b74a233a52024e114a334f
thiago lopes de mello <timabuntu@gmail.com>
2022-08-11 14:40:43 -0300

    feat: inserted form-container

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,18 @@
 import { Play } from 'phosphor-react'
-import { HomeContainer } from './styles'
+import { FormContainer, HomeContainer } from './styles'
 
 export function Home() {
   return (
     <HomeContainer>
       <form>
-        <div>
+        <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <input id="task" />
 
           <label htmlFor="">durante</label>
           <input type="number" id="minutesAmount" />
           <span>minutos.</span>
-        </div>
+        </FormContainer>
 
         <div>
           <span>0</span>
