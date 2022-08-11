f1e622aec3a3cf7d59aa074874900c45354b93eb
thiago lopes de mello <timabuntu@gmail.com>
2022-08-11 14:50:46 -0300

    style: stylized home-container dimensions and form

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,18 @@
 import styled from 'styled-components'
 
-export const HomeContainer = styled.div``
+export const HomeContainer = styled.div`
+  flex: 1;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 3.5rem;
+  }
+`
+export const FormContainer = styled.div``
+export const CountDownContainer = styled.div``
