f8c7f9b237884863c3c9f8a14046dae6ba93fc17
thiago lopes de mello <timabuntu@gmail.com>
2022-08-11 14:34:53 -0300

    feat: created elements form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,25 @@ import { HomeContainer } from './styles'
 export function Home() {
   return (
     <HomeContainer>
-      <h1>Home</h1>
+      <form>
+        <div>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" />
+
+          <label htmlFor="">durante</label>
+          <input type="number" id="minutesAmount" />
+          <span>minutos.</span>
+        </div>
+
+        <div>
+          <span>0</span>
+          <span>0</span>
+          <span>:</span>
+          <span>0</span>
+          <span>0</span>
+        </div>
+        <button type="submit">Começar</button>
+      </form>
     </HomeContainer>
   )
 }
