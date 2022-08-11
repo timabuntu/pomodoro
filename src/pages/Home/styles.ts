6866b0b437ac6a55ba1d6c35e65be26838c57e7b
thiago lopes de mello <timabuntu@gmail.com>
2022-08-11 15:43:28 -0300

    style: stylized separator dimensions and color

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -42,3 +42,13 @@ export const CountDownContainer = styled.div`
     border-radius: 8px;
   }
 `
+
+export const Separator = styled.div`
+  padding: 2rem 0;
+  color: ${(props) => props.theme['red-500']};
+
+  width: 4rem;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+`
