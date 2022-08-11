92704d465f8ac54163d475d4c9bafa7c43047e45
thiago lopes de mello <timabuntu@gmail.com>
2022-08-11 15:34:48 -0300

    style: stylized count-down-container dimensions and colors

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -26,4 +26,18 @@ export const FormContainer = styled.div`
   font-weight: bold;
   flex-wrap: wrap;
 `
-export const CountDownContainer = styled.div``
+export const CountDownContainer = styled.div`
+  font-family: 'Roboto', sans-serif;
+  font-size: 10rem;
+  line-height: 8rem;
+  color: ${(props) => props.theme['gray-100']};
+
+  display: flex;
+  gap: 1rem;
+
+  span {
+    background: ${(props) => props.theme['gray-700']};
+    padding: 2rem 1rem;
+    border-radius: 8px;
+  }
+`
