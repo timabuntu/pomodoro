21bd4d52c85e9be5c340243b1843084ec668557c
thiago lopes de mello <timabuntu@gmail.com>
2022-08-18 15:53:18 -0300

    style: stylized size elements container and list

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -1,5 +1,20 @@
 import styled from 'styled-components'
 
-export const HistoryContainer = styled.main``
+export const HistoryContainer = styled.main`
+  flex: 1;
+  padding: 3.5rem;
 
-export const HistoryList = styled.div``
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    font-size: 1.5rem;
+    color: ${(props) => props.theme['gray-100']};
+  }
+`
+
+export const HistoryList = styled.div`
+  flex: 1;
+  overflow: auto;
+  margin-top: 2rem;
+`
