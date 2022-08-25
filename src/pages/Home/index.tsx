b83b4ef608f92a96909757bd4825384e4a91a0ed
thiago lopes de mello <timabuntu@gmail.com>
2022-08-25 11:00:08 -0300

    feat: rendered the minutes and seconds

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -105,11 +105,11 @@ export function Home() {
         </FormContainer>
 
         <CountDownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </CountDownContainer>
 
         <StartCountdownButton disabled={isSubmitDisabled} type="submit">
