0f518be9b210e5d44fc4644114e650a34960dbe6
thiago lopes de mello <timabuntu@gmail.com>
2022-08-25 10:52:46 -0300

    feat: rounds current seconds to floor and converts to minutes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -64,6 +64,8 @@ export function Home() {
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
 
+  const minutesAmount = Math.floor(currentSeconds / 60)
+
   const task = watch('task')
   const isSubmitDisabled = !task
 
